3a1dcebf8fe2748f5707d9f90e01e5b1194bcfb2
meltonanimalcentre-cloud <meltonanimalcentre@gmail.com>
2026-01-01 18:11:27 +0000

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,3 @@
-export const metadata = {
-  title: "Bellars Grooming",
-  description: "Premium grooming with a calm, caring touch",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
+app/page.jsx
+import React ...
+export default function ...
